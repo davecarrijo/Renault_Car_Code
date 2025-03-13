54750085238e939b8aaa55966a94b4203bdb22bc
David Carrijo <49088508+davecarrijo@users.noreply.github.com>
2025-03-13 03:58:43 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Renault_Car_Code",
+  base: process.env.VITE_BASE_PATH || "/Renault_Car_Code",
 });
